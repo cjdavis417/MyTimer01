664eaecf686e2172eb17e265133da4d333ed2b16
Chris Davis <christopherjdavis417@gmail.com>
2020-05-09 19:53:29 -0700

    Add project files.

---
+++ MyTimer01/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Timers;
+
+namespace MyTimer01
+{
+    class Program
+    {
+        private static Timer timer;
+
+        static void Main(string[] args)
+        {
+            timer = new Timer()
+            {
+                Interval = 1000,
+            };
+            
+            timer.Elapsed += TimerEvent;
+            timer.AutoReset = true;
+            timer.Enabled = true;
+            Console.ReadLine();
+
+           
+        }
+
+        static void TimerEvent(object source, System.Timers.ElapsedEventArgs e)
+        {
+            Console.Clear();
+            Console.WriteLine("Test: {0:HH:mm:ss}", e.SignalTime);
+            
+        }
+
+    }
+}
